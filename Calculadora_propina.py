ca5184e363363af33a3321a29b6eaaf06aa2274a
Leonardo <leonardojim321@gmail.com>
2026-03-02 15:02:55 -0500

    agregue nueva branch

--- Calculadora_propina.py
+++ Calculadora_propina.py
@@ -1,17 +1,14 @@
-nombre =(input("Ingrese su nombre: "))
+nombre = input("Ingrese su nombre: ")
 precioComida = int(input("Ingrese el precio de su comida: "))
 
 if precioComida < 20:
     propina = precioComida * 0.10
-    
-elif precioComida >=20 <=50:
+
+elif precioComida >= 20 <= 50:
     propina = precioComida * 0.15
 else:
     propina = precioComida * 0.20
-    
-total =precioComida+propina
 
-print(f"{nombre} debe pagar de propina un {propina}%, y su total es de {int(total)}\n")
-    
-    
-    
+total = precioComida + propina
+
+print(f"{nombre} debe pagar de propina un {propina}%, y su total es de {int(total)}$\n")
